b399330597903141fa093643d3708145e105602f
Andrey Svezhincev <starscream902@gmail.com>
2019-04-07 13:11:56 +0300

    done terraform-1 homework

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-north1"
+}
+
+variable zone {
+  description = "zone"
+
+  # Значение по умолчанию
+  default = "europe-north1-c"
+}
+
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}

65915b0682e0ad1786bfb14b1839aba8ea19fbf1
Andrey Svezhincev <starscream902@gmail.com>
2019-04-07 13:52:46 +0300

    fix for passing check

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,83 +1,76 @@
 terraform {
-  # Версия terraform
-  required_version = "0.11.13"
+	# Версия terraform
+	# required_version = "0.11.13"
+	required_version = "0.11.7"
 }
 
 provider "google" {
-  # Версия провайдера
-  version = "2.0.0"
-
-  # ID проекта
-  # project = "corded-aquifer-235018"
-  project = "${var.project}"
-
-  # region = "europe-north1"
-  region = "${var.region}"
+	# Версия провайдера
+	version = "2.0.0"
+	# ID проекта
+	# project = "corded-aquifer-235018"
+	project = "${var.project}"
+	# region = "europe-north1"
+	region = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "europe-north1-c"
-  tags         = ["reddit-app"]
+	name = "reddit-app"
+	machine_type = "g1-small"
+	# zone = "europe-north1-c"
+	zone = "${var.zone}"
+	tags = ["reddit-app"]
+	# определение загрузочного диска
+	boot_disk {
+		initialize_params {
+			#image = "reddit-base"
+			image = "${var.disk_image}"
+		}
+	}
+	# определение сетевого интерфейса
+	network_interface {
+		# сеть, к которой присоединить данный интерфейс
+		network = "default"
+		# использовать ephemeral IP для доступа из Интернет
+		access_config {}
+	}
+	
+	metadata {
+		# путь до публичного ключа
+		# ssh-keys = "starscream902:${file("~/.ssh/GitHub-StarScream902-pub")}"
+		ssh-keys = "starscream902:${file(var.public_key_path)}"
+	}
 
-  # определение загрузочного диска
-  boot_disk {
-    initialize_params {
-      #image = "reddit-base"
-      image = "${var.disk_image}"
-    }
-  }
+	connection {
+		type = "ssh"
+		user = "starscream902"
+		agent = false
+		# путь до приватного ключа
+		# private_key = "${file("~/.ssh/GitHub-StarScream902-priv.OpenSSH")}"
+		private_key = "${file(var.private_key_path)}"
+	}
 
-  # определение сетевого интерфейса
-  network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
+	provisioner "file" {
+		source = "files/puma.service"
+		destination = "/tmp/puma.service"
+	}
 
-    # использовать ephemeral IP для доступа из Интернет
-    access_config {}
-  }
-
-  metadata {
-    # путь до публичного ключа
-    # ssh-keys = "starscream902:${file("~/.ssh/GitHub-StarScream902-pub")}"
-    ssh-keys = "starscream902:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    user  = "starscream902"
-    agent = false
-
-    # путь до приватного ключа
-    private_key = "${file("~/.ssh/GitHub-StarScream902-priv.OpenSSH")}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+	provisioner "remote-exec" {
+		script = "files/deploy.sh"
+	}
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  # Название сети, в которой действует правило
-  network = "default"
-
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
+	name = "allow-puma-default"
+	# Название сети, в которой действует правило
+	network = "default"
+	# Какой доступ разрешить
+	allow {
+		protocol = "tcp"
+		ports = ["9292"]
+	}
+	# Каким адресам разрешаем доступ
+	source_ranges = ["0.0.0.0/0"]
+	# Правило применимо для инстансов с перечисленными тэгами
+	target_tags = ["reddit-app"]
 }
